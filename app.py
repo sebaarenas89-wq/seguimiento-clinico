9dd084b1add7765157811c8f9f1e45d99aa05b94
sebaarenas89-wq <seba.arenas.89@gmail.com>
2026-07-02 08:38:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ def formatear_fecha(fecha):
 
 
 def evaluar_alerta_atm(fila):
-    estado = fila.get("estado", "")
+    estado = str(fila.get("estado", "")).replace("🔵", "").strip()
     fecha_termino = fila.get("fecha_termino", None)
 
-    if fecha_termino:
+    if fecha_termino is not None and str(fecha_termino).strip() not in ["", "NaT", "None"]:
         fecha_termino_dt = pd.to_datetime(fecha_termino).date()
         if fecha_termino_dt < date.today():
             return "⚫ Finalizado"
