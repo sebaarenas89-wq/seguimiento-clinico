42b969bdca0eaab0bb678579d10889090ed4f354
sebaarenas89-wq <seba.arenas.89@gmail.com>
2026-07-08 16:19:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -200,15 +200,15 @@ def crear_tablas():
     conn.close()
    
 
-def guardar_paciente(nombre, id_paciente, servicio, fecha_ingreso, diagnosticos, antecedentes):
+def guardar_paciente(nombre, id_paciente, servicio, fecha_ingreso, diagnosticos, antecedentes, usuario_id):
     conn = conectar_db()
     cursor = conn.cursor()
     cursor.execute("""
         INSERT INTO pacientes (
-            nombre, id_paciente, servicio, fecha_ingreso, diagnosticos, antecedentes
+            nombre, id_paciente, servicio, fecha_ingreso, diagnosticos, antecedentes, usuario_id
         )
-        VALUES (%s, %s, %s, %s, %s, %s)
-    """, (nombre, id_paciente, servicio, str(fecha_ingreso), diagnosticos, antecedentes))
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+    """, (nombre, id_paciente, servicio, str(fecha_ingreso), diagnosticos, antecedentes, int(usuario_id)))
     conn.commit()
     conn.close()
 
