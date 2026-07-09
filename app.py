c15c84aa4d1d16b6f05741109a6fa0b2420847a9
sebaarenas89-wq <seba.arenas.89@gmail.com>
2026-07-09 17:13:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -227,7 +227,7 @@ def guardar_paciente(nombre, id_paciente, servicio, cama, fecha_ingreso, diagnos
     cursor = conn.cursor()
     cursor.execute("""
         INSERT INTO pacientes (
-            nombre, id_paciente, servicio, fecha_ingreso, diagnosticos, antecedentes, usuario_id
+            nombre, id_paciente, servicio, cama, fecha_ingreso, diagnosticos, antecedentes, usuario_id
         )
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
     """, (nombre, id_paciente, servicio, cama, str(fecha_ingreso), diagnosticos, antecedentes, int(usuario_id)))
